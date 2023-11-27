d4ad7f8cb2cba42ab45550f0611cbc91779446fc
HaeChan0305 <kim.haechan@kaist.ac.kr>
2023-11-27 18:42:36 +0000

    models/Alignment/main.py ALL COMPLETEgit add Alignment/!

--- models/Alignment/main.py
+++ models/Alignment/main.py
@@ -4,28 +4,7 @@ import torch
 import open3d as o3d
 from PIL import Image
 import scipy
-
-
-#############
-# depth_to_points 분석을 통해, object별 (z_max - z_min) 값을 찾아야 됨.
-#   - depth_to_mesh의 view vector = [0, 0, -1]
-#   - 배경이 min, max 값에 영향을 미칠 수 있으므로, mask를 씌워야됨.
-#############
-# def get_scene_gap(depth_to_points, mask):
-#     z_points = depth_to_points[:, :, 2]
-    
-#     max_mask = np.where(mask==255, z_points, -np.inf)
-#     z_max = np.max(max_mask)
-    
-#     min_mask = np.where(mask==255, z_points, np.inf)
-#     z_min = np.min(min_mask)
-    
-#     return z_max - z_min
-
-# z_gap0 = get_scene_gap(depth_to_points, mask0)
-# z_gap1 = get_scene_gap(depth_to_points, mask1)
-# print(z_gap0)
-# print(z_gap1)
+import os
 
 #############
 # Rotation
@@ -70,15 +49,16 @@ def masking(mask, depth_to_points):
     Return :
         masked_points : np.array(M, 3)
     '''
+    H, W = mask.shape
+    H_, W_, _ = depth_to_points.shape
+    assert(H == H_ and W == W_)
     
-    masked_points = depth_to_points[np.where(mask==255)]
-    return masked_points
+    return depth_to_points[np.where(mask==255)]
     
 
 #############
 # Sampling Strategy
 #   - vertex norm이 view direction과 90도 이상 차이나는 vertex 제외
-#
 #############
 def vertex_normal_sampling(points, M):
     '''
@@ -95,7 +75,7 @@ def vertex_normal_sampling(points, M):
     projection = points @ view_direction
     sampled_points = points[np.where(projection > 0)]
     
-    print(">>> vertex_normal_sampling : ", points.shape[0], "->", sampled_points.shape[0])
+    print("\t\t>>> vertex_normal_sampling : ", points.shape[0], "->", sampled_points.shape[0])
     assert sampled_points.shape[0] > M
     return sampled_points
     
@@ -110,7 +90,7 @@ def uniform_sampling(points, M):
     sampled_point_cloud = point_cloud.uniform_down_sample(every_k_points=sampling_rate)
     sampled_points = np.array(sampled_point_cloud.points)
     
-    print(">>> uniform_sampling : ", points.shape[0], "->", sampled_points.shape[0])
+    print("\t\t>>> uniform_sampling : ", points.shape[0], "->", sampled_points.shape[0])
     return sampled_points
     
 
@@ -121,7 +101,7 @@ def random_sampling(points, M):
     indices = np.random.choice(N, size=M, replace=False)
     sampled_points = points[indices, :]
     
-    print(">>> random_sampling : ", points.shape[0], "->", sampled_points.shape[0])
+    print("\t\t>>> random_sampling : ", points.shape[0], "->", sampled_points.shape[0])
     return sampled_points
 
 
@@ -141,8 +121,8 @@ def sampling(mesh, masked_depth_to_points):
     #     sampled_points = random_sampling(masked_depth_to_points, N)
     
     # 연산량 감소하기 위한 코드
-    sampled_points = uniform_sampling(sampled_points, 10000)
-    sampled_depth_to_points = uniform_sampling(masked_depth_to_points, 10000)
+    sampled_points = uniform_sampling(sampled_points, 15000)
+    sampled_depth_to_points = uniform_sampling(masked_depth_to_points, 15000)
     N = sampled_points.shape[0]
     M = sampled_depth_to_points.shape[0]
     
@@ -150,7 +130,6 @@ def sampling(mesh, masked_depth_to_points):
         sampled_points = random_sampling(sampled_points, M)
     elif N < M:
         sampled_depth_to_points = random_sampling(sampled_depth_to_points, N)
-    
     
     assert sampled_points.shape[0] == sampled_depth_to_points.shape[0]
     return sampled_points, sampled_depth_to_points
@@ -164,12 +143,10 @@ def sampling(mesh, masked_depth_to_points):
 #   - d : (4, 4), X_를 scaling, translation 시키는 transformation matrix 
 #############
 def chamfer_distance(X, Y):
-    # print(">>> IN  : chamfer_distacne()")
     dist_x_to_y = cp.linalg.norm(X[:, None, :] - Y, axis=-1).min(axis=-1)
     dist_y_to_x = cp.linalg.norm(Y[:, None, :] - X, axis=-1).min(axis=-1)
     
     total_distance = cp.sum(dist_x_to_y) + cp.sum(dist_y_to_x)
-    # print(">>> OUT : chamfer_distacne()")
     return total_distance
 
 def params_to_matrix(params):
@@ -177,10 +154,12 @@ def params_to_matrix(params):
     params : np.array([s, tx, ty, tz])
     '''
     s, tx, ty, tz = params
-    return np.array([[s,   0,  0, 0],
-                     [0,   s,  0, 0],
-                     [0,   0,  s, 0],
-                     [tx, ty, tz, 1]])
+    s = np.abs(s)
+    assert s > 0
+    return np.array([[ s,  0,  0,  0],
+                     [ 0,  s,  0,  0],
+                     [ 0,  0,  s,  0],
+                     [tx, ty, tz,  1]])
 
 def objective_function(params, x, y):
     '''
@@ -206,23 +185,60 @@ def optimizing(x, y):
     '''
     x : sampled vertices in the mesh
     y : masked points in the depth_to_points
-    params : np.array([sx, sy, sz, tx, ty, tz]), six elements for transformation
+    params : np.array([s, tx, ty, tz]), six elements for transformation
     '''
     initial_params = np.random.randn(4)
     result = scipy.optimize.minimize(objective_function, initial_params, args=(x, y), method='BFGS')
     
     optimized_params = result.x
-    optimized_matrix = params_to_matrix(optimized_params)
-    return optimized_matrix
+    return optimized_params
+    
+#############
+# Applying
+#############
+def applying(mesh, optimized_params):
+    s, tx, ty, tz = optimized_params
+    s = np.abs(s)
+    
+    mesh.scale(s, center=(0, 0, 0))
+    mesh.translate((tx, ty, tz))
+    
+    return mesh
     
 
-def main():
+def process_per_object(mask, mesh, depth_to_points):
+    # Rotation
+    print("\t>>> Rotation ...")
+    rotated_mesh = rotation_alignment(mesh)
+    
+    # Masking
+    print("\t>>> Masking ...")
+    masked_points = masking(mask, depth_to_points)
+    
+    # Sampling
+    print("\t>>> Sampling ...")
+    sampled_points, sampled_masked_points = sampling(rotated_mesh, masked_points)
+    
+    # Optimizing
+    print("\t>>> Optimizing ...")
+    optimized_params = optimizing(sampled_points, sampled_masked_points)
+    
+    # Applying
+    print("\t>>> Applying ...")
+    result_mesh = applying(rotated_mesh, optimized_params)
+    
+    return result_mesh
+    
+
+def process(sample):
+    DIR = f"../../data/output/{sample}/"
+    
     # input으로 받을 수 있게 변경할 것.
-    mask0_path = "../../data/output/1/mask0.jpg"
-    mask1_path = "../../data/output/1/mask1.jpg"
-    mesh0_path = "../../data/output/1/mesh0.ply"
-    mesh1_path = "../../data/output/1/mesh1.ply"
-    depth_to_points_path = "../../data/output/1/depth.npy"
+    mask0_path = DIR + "mask0.jpg"
+    mask1_path = DIR + "mask1.jpg"
+    mesh0_path = DIR + "mesh0.ply"
+    mesh1_path = DIR + "mesh1.ply"
+    depth_to_points_path = DIR + "depth.npy"
 
     # Load data
     print(">>> Loading data ...")
@@ -232,39 +248,29 @@ def main():
     mesh1 = o3d.io.read_triangle_mesh(mesh1_path)
     depth_to_points = np.load(depth_to_points_path)
 
-    # Rotation
-    print(">>> Rotation ...")
-    rotated_mesh0 = rotation_alignment(mesh0)
-    rotated_mesh1 = rotation_alignment(mesh1)
-
-    o3d.io.write_triangle_mesh('../../data/output/1/rotated_mesh0.ply', rotated_mesh0)
-    o3d.io.write_triangle_mesh('../../data/output/1/rotated_mesh1.ply', rotated_mesh1)
-    
-    # Masking
-    print(">>> Masking ...")
-    masked_points0 = masking(mask0, depth_to_points)
-    masked_points1 = masking(mask1, depth_to_points)
-    
-    # Sampling
-    print(">>> Sampling ...")
-    sampled_points0, sampled_masked_points0 = sampling(rotated_mesh0, masked_points0)
-    sampled_points1, sampled_masked_points1 = sampling(rotated_mesh1, masked_points1)
-    
-    # Optimizing
-    print(">>> Optimizing ...")
-    optimized_matrix0 = optimizing(sampled_points0, sampled_masked_points0)
-    optimized_matrix1 = optimizing(sampled_points1, sampled_masked_points1)
-    
-    # Applying
-    print(">>> Applying ...")
-    result_mesh0 = rotated_mesh0.transform(optimized_matrix0)
-    result_mesh1 = rotated_mesh1.transform(optimized_matrix1)
+    # Process
+    print(">>> Object 0 ...")
+    result_mesh0 = process_per_object(mask0, mesh0, depth_to_points)
+    print(">>> Object 1 ...")
+    result_mesh1 = process_per_object(mask1, mesh1, depth_to_points)
+    merged_mesh = result_mesh0 + result_mesh1
     
     # Exporting
     print(">>> Exporting ...")
-    output0 = "../../data/output/1/result_mesh0.ply"
-    output1 = "../../data/output/1/result_mesh1.ply"
-    o3d.io.write_triangle_mesh(output0, result_mesh0)
-    o3d.io.write_triangle_mesh(output1, result_mesh1)
+    o3d.io.write_triangle_mesh(DIR + "result_mesh0.ply", result_mesh0)
+    o3d.io.write_triangle_mesh(DIR + "result_mesh1.ply", result_mesh1)
+    o3d.io.write_triangle_mesh(DIR + "result_merged_mesh.ply", merged_mesh)
     
+    
+def main():
+    samples = [1, 4, 6, 9, 10, 12, 13, 14, 15, 19, 22]
+    for sample in samples:
+        print(f">>> Start sample {sample}")
+        try:
+            process(sample)
+            print(f">>> SUCCESS : sample {sample}")
+        except Exception as e:
+            print(e)
+            print(f">>> FAIL : sample {sample}")
+
 main()
